c5a9962d0678c42c48f081976f89f0788a24b5ad
Eduardo4532 <pool42708@gmail.com>
2024-06-02 15:06:38 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
-global.botnumber = ""
+global.botnumber = "523345425784"
 global.confirmCode = ""
 
 global.owner = [
